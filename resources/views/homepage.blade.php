a3f22df402ff4691d7a1a30477722d10ee5dca30
Sanka Udeshika <147336157+SankaUdeshika@users.noreply.github.com>
2024-10-04 18:19:11 +0530

    Done Programming Payment Histroy

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -94,8 +94,14 @@ if (isset($_SESSION["user"])) {
                                         <div class="nav nav-tabs" role="tablist">
                                             <a class="nav-item nav-link active" data-bs-toggle="tab" href="#nav-account" role="tab" aria-selected="false"><i class="fas fa-user"></i>Password Change</a>
                                             <a class="nav-item nav-link" data-bs-toggle="tab" href="#nav-add-product" role="tab" aria-selected="true"><i class="fas fa-mobile-alt"></i>Account Details</a>
-                                            <a class="nav-item nav-link" data-bs-toggle="tab" href="#nav-product-listing" role="tab" aria-selected="false"><i class="fas fa-list"></i>OLCA Members</a>
-                                            <a class="nav-item nav-link" data-bs-toggle="tab" href="#nav-orders" role="tab" aria-selected="false"><i class="fas fa-shopping-basket"></i>Orders</a>
+                                            <?php
+                                            if ($user_session["user_role_role_id"] == "1") {
+                                            ?>
+                                                <a class="nav-item nav-link" data-bs-toggle="tab" href="#nav-product-listing" role="tab" aria-selected="false"><i class="fas fa-list"></i>OLCA Members</a>
+                                                <a class="nav-item nav-link" data-bs-toggle="tab" href="#nav-orders" role="tab" aria-selected="false"><i class="fas fa-shopping-basket"></i>payment history</a>
+                                            <?php
+                                            }
+                                            ?>
                                             <a class="nav-item nav-link" data-bs-toggle="tab" href="#nav-address" role="tab" aria-selected="false"><i class="fas fa-home"></i>Make Payments</a>
                                             <a class="nav-item nav-link" href="#"><i class="fal fa-sign-out"></i>Logout</a>
                                         </div>
@@ -183,7 +189,7 @@ if (isset($_SESSION["user"])) {
 
                                                     <div class="col-12">
                                                         <div class="form-group mb--0 mt--30 d-grid">
-                                                            <input type="button" onclick="updateProfile();" class="axil-btn" value="Add Product To Listing">
+                                                            <input type="button" onclick="updateProfile();" class="axil-btn" value="Update Profile Infomations">
                                                         </div>
                                                     </div>
                                                 </div>
@@ -195,26 +201,41 @@ if (isset($_SESSION["user"])) {
                                     <div class="tab-pane fade" id="nav-orders" role="tabpanel">
                                         <div class="axil-dashboard-order">
                                             <div class="table-responsive">
+                                                <div class="addrss-header d-flex align-items-center justify-content-between">
+                                                    <h4 class="title mb-0">Payment History</h4>
+                                                    <a href="#" class="address-edit"><i class="far fa-edit"></i></a>
+                                                </div>
                                                 <table class="table">
                                                     <thead>
                                                         <tr>
-                                                            <th scope="col">Order</th>
-                                                            <th scope="col">Products</th>
+                                                            <th scope="col">payment ID</th>
+                                                            <th scope="col"> Email</th>
+                                                            <th scope="col"> Name</th>
+                                                            <th scope="col">Amount</th>
                                                             <th scope="col">Date</th>
-                                                            <th scope="col">Status</th>
-                                                            <th scope="col">Total</th>
-                                                            <th scope="col">Actions</th>
+                                                            <th scope="col">Time</th>
+                                                            <th scope="col">Description</th>
                                                         </tr>
                                                     </thead>
                                                     <tbody>
-                                                        <tr>
-                                                            <th scope="row">#1234</th>
-                                                            <td>Apple iPhone 15 Pro <br /> Samsung Galaxy Note 20</td>
-                                                            <td>September 02, 2024</td>
-                                                            <td>Delivered</td>
-                                                            <td>Rs 0.00</td>
-                                                            <td><a href="#" class="axil-btn view-btn">View</a></td>
-                                                        </tr>
+                                                        <?php
+                                                        $payment_rs =  Database::search("SELECT * FROM `payments` INNER JOIN `user` ON `user`.`email` = `payments`.`user_email` ");
+                                                        $payment_num = $payment_rs->num_rows;
+                                                        for ($x = 0; $x < $payment_num; $x++) {
+                                                            $payment_data = $payment_rs->fetch_assoc();
+                                                        ?>
+                                                            <tr>
+                                                                <td><?php echo ($payment_data["payment_id"]) ?></td>
+                                                                <th scope="row"><?php echo ($payment_data["email"]) ?></th>
+                                                                <td><?php echo ($payment_data["with_initials_nae"]) ?></td>
+                                                                <td><?php echo ($payment_data["amount"]) ?></td>
+                                                                <td><?php echo ($payment_data["payment_date"]) ?></td>
+                                                                <td><?php echo ($payment_data["payment_time"]) ?></td>
+                                                                <td><?php echo ($payment_data["Descriptions"]) ?></td>
+                                                            </tr>
+                                                        <?php
+                                                        }
+                                                        ?>
                                                     </tbody>
                                                 </table>
                                             </div>
@@ -223,6 +244,10 @@ if (isset($_SESSION["user"])) {
                                     <div class="tab-pane fade" id="nav-product-listing" role="tabpanel">
                                         <div class="axil-dashboard-order">
                                             <div class="table-responsive">
+                                                <div class="addrss-header d-flex align-items-center justify-content-between">
+                                                    <h4 class="title mb-0">Members List</h4>
+                                                    <a href="#" class="address-edit"><i class="far fa-edit"></i></a>
+                                                </div>
                                                 <table class="table">
                                                     <thead>
                                                         <tr>
@@ -367,169 +392,7 @@ if (isset($_SESSION["user"])) {
         </main>
 
 
-        <div class="service-area">
-            <div class="container">
-                <div class="row row-cols-xl-4 row-cols-sm-2 row-cols-1 row--20">
-                    <div class="col">
-                        <div class="service-box service-style-2">
-                            <div class="icon">
-                                <img src="./assets/images/icons/service1.png" alt="Service">
-                            </div>
-                            <div class="content">
-                                <h6 class="title">Fast &amp; Secure Delivery</h6>
-                                <p>Tell about your service.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="service-box service-style-2">
-                            <div class="icon">
-                                <img src="./assets/images/icons/service2.png" alt="Service">
-                            </div>
-                            <div class="content">
-                                <h6 class="title">Money Back Guarantee</h6>
-                                <p>Within 10 days.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="service-box service-style-2">
-                            <div class="icon">
-                                <img src="./assets/images/icons/service3.png" alt="Service">
-                            </div>
-                            <div class="content">
-                                <h6 class="title">24 Hour Return Policy</h6>
-                                <p>No question ask.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="service-box service-style-2">
-                            <div class="icon">
-                                <img src="./assets/images/icons/service4.png" alt="Service">
-                            </div>
-                            <div class="content">
-                                <h6 class="title">Pro Quality Support</h6>
-                                <p>24/7 Live support.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Start Footer Area  -->
-        <footer class="axil-footer-area footer-style-2">
-            <!-- Start Footer Top Area  -->
-            <div class="footer-top separator-top">
-                <div class="container">
-                    <div class="row">
-                        <!-- Start Single Widget  -->
-                        <div class="col-lg-3 col-sm-6">
-                            <div class="axil-footer-widget">
-                                <h5 class="widget-title">Support</h5>
-
-                                <div class="inner">
-                                    <p>No.3B 1/4, <br>
-                                        Sri Sambuddhathva Jayanthi Mawatha, Colombo 00500, <br>
-                                        Sri Lanka.
-                                    </p>
-                                    <ul class="support-list-item">
-                                        <li><a href="mailto:support@smarttrade.lk"><i class="fal fa-envelope-open"></i> support@smarttrade.lk</a></li>
-                                        <li><a href="tel:(+94)112 506 000"><i class="fal fa-phone-alt"></i> (+94) 112 506 000</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- End Single Widget  -->
-                        <!-- Start Single Widget  -->
-                        <div class="col-lg-3 col-sm-6">
-                            <div class="axil-footer-widget">
-                                <h5 class="widget-title">Account</h5>
-                                <div class="inner">
-                                    <ul>
-                                        <li><a href="#">My Account</a></li>
-                                        <li><a href="#">Login / Register</a></li>
-                                        <li><a href="#">Cart</a></li>
-                                        <li><a href="#">Wishlist</a></li>
-                                        <li><a href="#">Shop</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- End Single Widget  -->
-                        <!-- Start Single Widget  -->
-                        <div class="col-lg-3 col-sm-6">
-                            <div class="axil-footer-widget">
-                                <h5 class="widget-title">Quick Link</h5>
-                                <div class="inner">
-                                    <ul>
-                                        <li><a href="#">Privacy Policy</a></li>
-                                        <li><a href="#">Terms Of Use</a></li>
-                                        <li><a href="#">FAQ</a></li>
-                                        <li><a href="#">Contact</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- End Single Widget  -->
-                        <!-- Start Single Widget  -->
-                        <div class="col-lg-3 col-sm-6">
-                            <div class="axil-footer-widget">
-                                <h5 class="widget-title">Download App</h5>
-                                <div class="inner">
-                                    <div class="download-btn-group">
-
-                                        <div class="app-link">
-                                            <a href="#">
-                                                <img src="assets/images/others/app-store.png" alt="App Store">
-                                            </a>
-                                            <a href="#">
-                                                <img src="assets/images/others/play-store.png" alt="Play Store">
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- End Single Widget  -->
-                    </div>
-                </div>
-            </div>
-            <!-- End Footer Top Area  -->
-            <!-- Start Copyright Area  -->
-            <div class="copyright-area copyright-default separator-top">
-                <div class="container">
-                    <div class="row align-items-center">
-                        <div class="col-xl-4">
-                            <div class="social-share">
-                                <a href="#"><i class="fab fa-facebook-f"></i></a>
-                                <a href="#"><i class="fab fa-instagram"></i></a>
-                                <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
-                                <a href="#"><i class="fab fa-linkedin-in"></i></a>
-                            </div>
-                        </div>
-                        <div class="col-xl-4 col-lg-12">
-                            <div class="copyright-left d-flex flex-wrap justify-content-center">
-                                <ul class="quick-link">
-                                    <li>© 2024. All rights reserved by <a target="_blank" href="#">Java Institute</a>.</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-xl-4 col-lg-12">
-                            <div class="copyright-right d-flex flex-wrap justify-content-xl-end justify-content-center align-items-center">
-                                <span class="card-text">Accept For</span>
-                                <ul class="payment-icons-bottom quick-link">
-                                    <li><img src="assets/images/icons/mastercard.png" alt="paypal cart"></li>
-                                    <li><img src="assets/images/icons/visa.png" alt="paypal cart"></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- End Copyright Area  -->
-        </footer>
-        <!-- End Footer Area  -->
+        
 
         <!-- Header Search Modal End -->
         <div class="header-search-modal" id="header-search-modal">
